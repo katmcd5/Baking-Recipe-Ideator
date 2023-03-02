e795a3c19d335537c837552499a0e34e4d7bf5e5
Katrina McDonagh <katrina@Katrinas-MacBook-Pro.local>
2023-03-01 21:02:25 -0800

    pop up done

--- client/main.js
+++ client/main.js
@@ -1,7 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   createCheckbox();
-  // document.querySelector('body').insertBefore(box, document.querySelector('form'))
 });
 
 const ingredients = [
@@ -21,6 +20,7 @@ const ingredients = [
 ]
 
 function createCheckbox() {
+  //create container
   const box = document.getElementById('box');
 
   const question = document.createElement('div');
@@ -32,42 +32,48 @@ function createCheckbox() {
   for (let ing of ingredients) { //create checkbox divs
     const miniBox = document.createElement('div');
     miniBox.id = 'miniBox';
-    miniBox.setAttribute('class', 'ingredient')
+    miniBox.setAttribute('class', 'ingredient');
+    //create checkbox itself
     let checkbox = document.createElement('input');
     checkbox.setAttribute('type', 'checkbox');
     checkbox.classList.add('ingredient');
     checkbox.name = ing;
     checkbox.id = ing;
+    //create label for each checkbox
     let boxLabel = document.createElement('label');
-    boxLabel.htmlFor = ing;
-    const text = document.createTextNode(`${ing}`)
+    boxLabel.htmlFor = ing; //link label to checkbox
+    const text = document.createTextNode(`${ing}`);
+
+    //append it all
     boxLabel.appendChild(text);
     miniBox.appendChild(checkbox);
     miniBox.appendChild(boxLabel);
     form.appendChild(miniBox);
   }
+  //append form to box container
   box.appendChild(form);
-  // form.action = '/ingredient';
-  // form.method = 'POST';
 
   function submitButton() {
+    //create submit button
     const submit = document.createElement('button');
     submit.setAttribute('type', 'submit');
     submit.id = 'submitButton';
-    // submit.setAttribute('onclick', onClick(event))
+    //create button label
     const text = document.createTextNode('Submit');
     submit.appendChild(text);
     const linebreak = document.createElement("br");
+    //append it all
     form.appendChild(linebreak);
     form.appendChild(submit);
     form.addEventListener('submit', onClick);
   }
+  //call function
   submitButton();
 }
 
 
 function onClick(e) {
-    e.preventDefault();
+    e.preventDefault(); //prevents auto refresh
     let checked = document.querySelectorAll('.ingredient');
     let ingredients = [];
     for (let checkbox of checked) {
@@ -85,26 +91,45 @@ function onClick(e) {
     })
       .then(data => data.json())
       .then(data => recipePop(data))
-      // .then(data => alert(data[0].name, data[0].link))
     
 }
 
 function recipePop(obj) {
+  if (obj.length === 0) alert('No recipes match! Please restock your ingredients.');
   const name = obj[0].name;
   const link = obj[0].link;
   const food = obj[0].image;
+
+  //create pop up div
   const recipe = document.createElement('div');
   recipe.setAttribute('id', 'recipe');
+
+  //create delete button on pop up
+  const deleteButton = document.createElement('button');
+  deleteButton.setAttribute('class', 'delete');
+  const text = document.createTextNode('X');
+  deleteButton.appendChild(text);
+  deleteButton.addEventListener('click', () => recipe.remove());
+  recipe.appendChild(deleteButton);
+
+  //create hyperlink div
+  const tagBox = document.createElement('div');
+  tagBox.setAttribute('class', 'link');
+  //create hyperlink
   const tag = document.createElement('a');
   tag.setAttribute('href', `${link}`);
   tag.setAttribute('target', '_blank');
   tag.innerText = `You can make ${name}!`;
-  recipe.appendChild(tag);
+  tagBox.appendChild(tag);
+  recipe.appendChild(tagBox);
+
+  //create image div
   const image = document.createElement('img');
   image.setAttribute('src', `${food}`);
   image.setAttribute('class', 'image');
   recipe.appendChild(image);
   document.body.appendChild(recipe);
+
 }
 
 
